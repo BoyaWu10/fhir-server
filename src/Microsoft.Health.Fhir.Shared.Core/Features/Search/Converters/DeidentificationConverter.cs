fc5e6602c30313f055523fdc9ebdce5cf197a4d1
FAREAST\boywu <boywu@microsoft.com>
2019-12-10 05:59:01 +0800

    demo of de-identification

---
+++ src/Microsoft.Health.Fhir.Shared.Core/Features/Search/Converters/DeidentificationConverter.cs
@@ -0,0 +1,87 @@
+﻿// -------------------------------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
+// -------------------------------------------------------------------------------------------------
+
+using System;
+using System.Linq;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+
+namespace Microsoft.Health.Fhir.Shared.Core.Features.Search.Converters
+{
+    public class DeidentificationConverter
+    {
+        private readonly FhirJsonParser _parser = new FhirJsonParser();
+        private readonly FhirJsonSerializer _serializer = new FhirJsonSerializer();
+
+        private static void RedactHumanName(HumanName name)
+        {
+            name.Text = name.Text == null ? name.Text : string.Empty;
+            name.Family = name.Family == null ? name.Family : string.Empty;
+            name.Given = name.Given.Select(x => string.Empty);
+            name.Prefix = name.Prefix.Select(x => string.Empty);
+            name.Suffix = name.Suffix.Select(x => string.Empty);
+            name.Period = new Period();
+        }
+
+        private static void RedactTelecom(ContactPoint telecom)
+        {
+            telecom.Value = telecom.Value == null ? telecom.Value : string.Empty;
+            telecom.Period = new Period();
+        }
+
+        private static void RedactAddress(Address address)
+        {
+            address.Text = address.Text == null ? address.Text : string.Empty;
+            address.Line = address.Line.Select(x => string.Empty);
+            address.City = address.City == null ? address.City : string.Empty;
+            address.District = address.District == null ? address.District : string.Empty;
+            address.PostalCode = address.PostalCode == null ? address.PostalCode : string.Empty;
+            address.Period = new Period();
+        }
+
+        public static bool NeedDeidentification(string resourceType)
+        {
+            return string.Equals(resourceType, ResourceType.Patient.ToString(), StringComparison.Ordinal) ||
+                string.Equals(resourceType, ResourceType.Account.ToString(), StringComparison.Ordinal);
+        }
+
+        public string DeidentifyPatientData(string data)
+        {
+            Patient patient = _parser.Parse<Patient>(data);
+
+            // Redact name, telecom, birthdate and address
+            foreach (HumanName name in patient.Name)
+            {
+                RedactHumanName(name);
+            }
+
+            foreach (ContactPoint telecom in patient.Telecom)
+            {
+                RedactTelecom(telecom);
+            }
+
+            patient.BirthDate = patient.BirthDate == null ? patient.BirthDate : string.Empty;
+
+            foreach (Address address in patient.Address)
+            {
+                RedactAddress(address);
+            }
+
+            return _serializer.SerializeToString(patient);
+        }
+
+        public string DeidentifyAccountData(string data)
+        {
+            Account account = _parser.Parse<Account>(data);
+
+            foreach (Identifier identifier in account.Identifier)
+            {
+                identifier.Value = string.Empty;
+            }
+
+            return _serializer.SerializeToString(account);
+        }
+    }
+}
--- src/Microsoft.Health.Fhir.Shared.Core/Features/Search/SearchResourceHandler.cs
+++ src/Microsoft.Health.Fhir.Shared.Core/Features/Search/SearchResourceHandler.cs
@@ -3,12 +3,17 @@
 // Licensed under the MIT License (MIT). See LICENSE in the repo root for license information.
 // -------------------------------------------------------------------------------------------------
 
+using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using EnsureThat;
+using Hl7.Fhir.Model;
 using MediatR;
+using Microsoft.Health.Fhir.Core.Features.Persistence;
 using Microsoft.Health.Fhir.Core.Messages.Search;
 using Microsoft.Health.Fhir.Core.Models;
+using Microsoft.Health.Fhir.Shared.Core.Features.Search.Converters;
 
 namespace Microsoft.Health.Fhir.Core.Features.Search
 {
@@ -19,6 +24,7 @@ namespace Microsoft.Health.Fhir.Core.Features.Search
     {
         private readonly ISearchService _searchService;
         private readonly IBundleFactory _bundleFactory;
+        private readonly DeidentificationConverter _deidConverter = new DeidentificationConverter();
 
         /// <summary>
         /// Initializes a new instance of the <see cref="SearchResourceHandler"/> class.
@@ -41,9 +47,51 @@ namespace Microsoft.Health.Fhir.Core.Features.Search
 
             SearchResult searchResult = await _searchService.SearchAsync(message.ResourceType, message.Queries, cancellationToken);
 
-            ResourceElement bundle = _bundleFactory.CreateSearchBundle(searchResult);
+            if (!DeidentificationConverter.NeedDeidentification(message.ResourceType))
+            {
+                ResourceElement bundle = _bundleFactory.CreateSearchBundle(searchResult);
+                return new SearchResourceResponse(bundle);
+            }
+            else
+            {
+                var redactedEntries = new List<SearchResultEntry>();
+                foreach (SearchResultEntry entry in searchResult.Results)
+                {
+                    string redactedData = string.Empty;
+                    if (string.Equals(message.ResourceType, ResourceType.Patient.ToString(), StringComparison.Ordinal))
+                    {
+                        redactedData = _deidConverter.DeidentifyPatientData(entry.Resource.RawResource.Data);
+                    }
+                    else if (string.Equals(message.ResourceType, ResourceType.Account.ToString(), StringComparison.Ordinal))
+                    {
+                        redactedData = _deidConverter.DeidentifyAccountData(entry.Resource.RawResource.Data);
+                    }
 
-            return new SearchResourceResponse(bundle);
+                    var redactedEntry = new SearchResultEntry(
+                    new ResourceWrapper(
+                        entry.Resource.ResourceId,
+                        entry.Resource.Version,
+                        entry.Resource.ResourceTypeName,
+                        new RawResource(redactedData, FhirResourceFormat.Json),
+                        entry.Resource.Request,
+                        entry.Resource.LastModified,
+                        entry.Resource.IsDeleted,
+                        entry.Resource.SearchIndices,
+                        entry.Resource.CompartmentIndices,
+                        entry.Resource.LastModifiedClaims),
+                    entry.SearchEntryMode);
+                    redactedEntries.Add(redactedEntry);
+                }
+
+                var redactedSearchResult = new SearchResult(
+                    redactedEntries as IEnumerable<SearchResultEntry>,
+                    searchResult.UnsupportedSearchParameters,
+                    searchResult.UnsupportedSortingParameters,
+                    searchResult.ContinuationToken);
+
+                ResourceElement bundle = _bundleFactory.CreateSearchBundle(redactedSearchResult);
+                return new SearchResourceResponse(bundle);
+            }
         }
     }
 }
